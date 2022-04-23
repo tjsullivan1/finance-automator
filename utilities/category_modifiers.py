a0554029c6fef4803740b664b003a5466f743227
Tim Sullivan <timothyj.sullivan1@gmail.com>
2022-04-22 20:40:27 -0500

    updated category modifiers utility to work at the command line

--- utilities/category_modifiers.py
+++ utilities/category_modifiers.py
@@ -4,15 +4,58 @@ import json
 import click
 
 
-# TODO: update to accept filename as input
-with open('expense_categories.json') as file:
-    categories = json.load(file)
+@click.command()
+@click.option(
+    "-f",
+    "--file-path",
+    "file_path",
+    required=True,
+    default=None,
+    help="Path to the category file.",
+)
+@click.option(
+    "-c",
+    "--category",
+    required=True,
+    default=None,
+    help="Name of the expense category.",
+)
+@click.option(
+    "-s",
+    "--match-string",
+    "match_string",
+    default=None,
+    help="String to match in the previous category",
+)
+# TODO: add option and logic for add / remove
+def main(
+    file_path: str,
+    category: str,
+    match_string: str,
+):
+    with open(file_path) as file:
+        categories = json.load(file)
 
-# TODO: update to accept cmdline input for the category name
-cat_index = next((i for i, item in enumerate(categories) if item["name"] == "<category_name>"), None)
+    # TODO: Consider refactoring this into its own function
+    cat_index = next(
+        (i for i, item in enumerate(categories) if item["name"] == category), None
+    )
+
+    # TODO: Consider refactoring this into its own function
+    if cat_index != None:
+        strings = categories[cat_index].get("match_strings")
+        
+        if match_string:
+            if match_string.lower() in [string.lower() for string in strings]:
+                click.echo("Already in the list")
+            else:
+                strings.append(match_string)
+                categories[cat_index]["match_strings"] = strings
+
+                with open(file_path, "w") as file:
+                    file.write(json.dumps(categories, indent=4))
+                    file.close()
 
 
-# TODO: add logic to make allow this to inplace edit
-if cat_index:
-    strings = categories[cat_index].get('match_strings')
-    print(strings)
+if __name__ == "__main__":
+    main()
