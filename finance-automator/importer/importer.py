771f478624dbd127f4b906332f2ecb2e773ad162
Tim Sullivan <timothyj.sullivan1@gmail.com>
2022-04-22 21:40:58 -0500

    ignoring jupyter notebooks & updated importer with chase function

--- finance-automator/importer/importer.py
+++ finance-automator/importer/importer.py
@@ -1,3 +1,5 @@
+import csv
+
 def import_wells_fargo_transactions(statement_csv) -> list:
     # initializing the titles and rows list
     rows = []
@@ -30,6 +32,22 @@ def import_amex_transactions(statement_csv) -> list:
             # TODO: There's got to be a more pythonic way to do this. 
             rows.append({
                 'Date': row.get('Date'), 
+                'Amount': row.get('Amount'), 
+                'Description': row.get('Description')
+            })
+            
+    return rows
+
+
+def import_chase_transactions(statement_csv) -> list:
+    rows = []
+    
+    with open(statement_csv, 'r') as csvfile:
+        reader = csv.DictReader(csvfile, skipinitialspace=True)
+        for row in reader:
+            # TODO: There's got to be a more pythonic way to do this. 
+            rows.append({
+                'Date': row.get('Transaction Date'), 
                 'Amount': row.get('Amount'), 
                 'Description': row.get('Description')
             })
