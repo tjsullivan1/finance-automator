d796d3e575d35f2a9f476eab5679129419c2100d
Tim Sullivan <timothyj.sullivan1@gmail.com>
2022-04-26 20:20:05 -0500

    Main looping through current files

--- finance-automator/main.py
+++ finance-automator/main.py
@@ -6,8 +6,8 @@ from importer.importer import import_amex_transactions, import_chase_transaction
 from categorizer.categorizer import get_categories_from_file, loop_to_set_category
 
 delta_rows = import_amex_transactions('/home/tjs/finance-automator/data/amexdeltaytd.csv')
-plat_rows = [] # import_amex_transactions('/home/tjs/finance-automator/data/amexplatytd.csv')
-chase_rows = [] # import_chase_transactions('/home/tjs/finance-automator/data/chaseytd.CSV')
+plat_rows = import_amex_transactions('/home/tjs/finance-automator/data/amexplatytd.csv')
+chase_rows = import_chase_transactions('/home/tjs/finance-automator/data/chaseytd.CSV')
 wells_rows = import_wells_fargo_transactions('/home/tjs/finance-automator/data/wfytd.csv')
 
 transactions = list(itertools.chain(delta_rows, plat_rows, chase_rows, wells_rows))
